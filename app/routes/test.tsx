b60f74b6da45acdd106bac8d3cfe3c49517b6acc
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-11-18 16:30:30 +0900

    chore: add loader

---
+++ app/routes/test.tsx
@@ -0,0 +1,11 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export function loader() {
+  return json("hello loader data");
+}
+
+export default function Route() {
+  const loaderData = useLoaderData<typeof loader>();
+  return <div>{loaderData}</div>;
+}
